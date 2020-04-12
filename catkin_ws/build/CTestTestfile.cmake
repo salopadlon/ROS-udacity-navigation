ba6d7af2077cca8b96bdd37bf4fdff24e23d8f7e
salopadlon <p.sadlon13@gmail.com>
2020-04-12 15:16:28 +0000

    navigation

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -7,4 +7,3 @@
 subdirs(gtest)
 subdirs(my_robot)
 subdirs(ball_chaser)
-subdirs(pgm_map_creator)
--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,4 +8,3 @@
 
 # modified environment variables
 export PWD="/home/workspace/catkin_ws/build"
-export PYTHONPATH="/home/workspace/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
